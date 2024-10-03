2f694f1cb73f7c85ca3cfd65b009b1ecbad298d4
hoanglong <87950841+socialist-dev@users.noreply.github.com>
2024-10-04 02:05:30 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -91,7 +91,7 @@ if user_prompt:
     # Display response message of assistant
     with st.chat_message("assistant", avatar='https://pub-821312cfd07a4061bf7b99c1f23ed29b.r2.dev/v1/dynamic/color/flash-dynamic-color.png'):
         stream = client.chat.completions.create(
-            model="llama-3.1-8b-instant",
+            model="llama-3.1-70b-versatile",
             messages=messages,
             stream=True  # for streaming the message
         )
